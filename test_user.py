93a016853e47aa999a7face2418d882e38638a7b
juma-moringa <allan.juma@student.moringaschool.com>
2021-05-28 21:50:03 +0300

    added the user class

---
+++ test_user.py
@@ -0,0 +1,11 @@
+import unittest
+from user import User
+
+class TestUser(unittest.TestCase):
+  """
+  this is a subclass being created which inherits from unittest testcase
+  """  
+def setUp(self):
+    """
+    this method allows us to define instructions that will be executed before each test
+    """
