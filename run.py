0ea7d53076bd718c41d3553bc53c18f2f31973f3
juma-moringa <allan.juma@student.moringaschool.com>
2021-05-29 12:03:33 +0300

    validated the password and username and proceeds

--- run.py
+++ run.py
@@ -19,38 +19,59 @@ def check_existing_user(username, password):
     '''
     return User.user_exist(username, password)
 
-def main():
-    print('''
-    Hello user, welcome to LASTPASS where we help you remember your credentials
-    ''') 
-    print("Please enter your Username:") 
-    username = input()
+# def main():
+#     print('''
+#     Hello user, welcome to LASTPASS where we help you remember your credentials
+#     ''') 
+#     print("Please enter your Username:") 
+#     username = input()
 
-    print("Please enter your Password:") 
-    password =input()
+#     print("Please enter your Password:") 
+#     password =input()
 
 def main():    
-    print("Hello, Welcome to PassworLocker. Create an account to save your passwords")
-    print('Enter your username')
+    print("""
+    Hello, Welcome to LASTPASS. 
+    where we help you remember your credentials.
+    Create an account to save your passwords
+    """)
+    print('Enter your username:')
     username = input()
-    print("What's your password")
+    print("What's your password:")
     password = input()
-    print("Confirm password ....")
+    print("Confirm  your password ....")
     confirm_password = input()
     
     while confirm_password != password:
-                print("Password did not match")
+                print("Password did not match...")
                 print("password ....")
                 password = input()
-                print("Confirm password ....")
+                print("Confirm  your password ....")
+                confirm_password = input()
     else:
                 save_user(create_user(username, password))
-                print(f'Congratulations 🎉, New Account has been created for: {username} using password: {password}')
-                print("proceed to login")
-                print("username")
-                entered_first_name = input()
-                print("your password")
+                print(f'''
+                Congratulations, New Account has been created for: {username}
+                using password: ({password}).....
+                Keep it Last-Pass always.''')
+                print(" You can proceed to login")
+                print("Username:")
+                username = input()
+                print("Your password:")
                 entered_password = input()            
+    while username != username or entered_password != password:
+                print("Invalid username or password")
+                print('Username')
+                username = input()
+                print("Your password")
+                entered_password = input()
+    else:                
+        
+                print(f'''
+                Welcome back  {username}.
+
+                You can now proceed 
+                ''')
 
 if __name__ == '__main__':
     main()    
